236eab253bb2eb23ccd39c0d33b6265c7ba0ff23
Pavlos Elmatzidis <128632668+Elmatzidis@users.noreply.github.com>
2026-04-25 21:05:18 +0300

    Update HTML template path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,14 @@ module.exports = {
       title: "Webpack App",
       filename: "index.html",
       port:8080,
-      template: path.resolve(__dirname, "src/template.html"), 
+      template: path.resolve(__dirname, "src/index.html"), 
     }),
   ],
   devtool: "eval-source-map",
   devServer: {
     port:8080,
     open:true,
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/index.html"],
   },
 
   module: {
